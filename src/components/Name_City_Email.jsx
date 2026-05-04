374a1092c490e75e84da7e6d7584296a2c453a2f
Anton102967 <anton.puzderi@gmail.com>
2026-05-04 22:04:05 +0300

    JSX практика fix

--- src/components/Name_City_Email.jsx
+++ src/components/Name_City_Email.jsx
@@ -10,12 +10,15 @@ export function Email() {
     return <h1>My email Oleg@gmail.com</h1>
 }
 
+export function Phone() {
+    return <h1>My phone, Samsung</h1>;
+}
+
 export function Contacts() {
     return(
         <>
             <Email />
-            <h1>My phone, Samsung</h1>
+            <Phone />
         </>
-
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import {City, Contacts, Email, Name} from "../components/Name_City_Email.jsx";
+import {City, Contacts, Name} from "../components/Name_City_Email.jsx";
 
 function Home() {
     return (
         <>
             <Name />
             <City />
-            <Email />
             <Contacts />
         </>
 
