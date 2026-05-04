de1dcefda8f937920bcc3e3b8ea7760e8816e9f0
Anton102967 <anton.puzderi@gmail.com>
2026-05-04 15:23:08 +0300

    Практика, шаг 7: Обзор файловой структуры React-проекта fix

---
+++ src/api/fetchData.js
@@ -0,0 +1,5 @@
+function fetchData() {
+  console.log('API ready')
+}
+
+export default fetchData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import Header from "../components/Header.jsx";
+import fetchData from "../api/fetchData.js";
 
 function Home() {
-    return <Header title="Home" />
+    return (
+        <Header title="Home" onClick={fetchData()}/>
+    )
 }
 
 export default Home
