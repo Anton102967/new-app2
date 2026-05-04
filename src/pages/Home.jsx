0a150cdf4f8bacc93bb4764fd1ad9c02aa39f788
Anton102967 <anton.puzderi@gmail.com>
2026-05-04 15:06:55 +0300

    Практика, шаг 6: Обзор файловой структуры React-проекта fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header.jsx";
 import formatText from "../helpers/formatText.js";
 
 function Home() {
-    return formatText(<Header />)
+    return formatText(<Header />);
 }
 
 export default Home
