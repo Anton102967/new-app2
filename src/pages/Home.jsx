8efdb3fc2f59edcc9039ca7bccefa21da3908b71
Anton102967 <anton.puzderi@gmail.com>
2026-05-07 15:51:15 +0300

    props практика 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import {ProductCard} from "../components/Test.jsx";
 
 function Home() {
+    const products = [
+      { id: 1, name: 'Апельсины', price: 45 },
+      { id: 2, name: 'Яблоки', price: 60 },
+      { id: 3, name: 'Бананы', price: 80 },
+      { id: 4, name: 'Груши', price: 95 },
+    ];
     return(
         <>
-            <ProductCard name={'апельсины'} price={'45'}/>
+            {products.map(el => (
+                <ProductCard name={el.name} price={el.price} key={el.id} />
+            ))}
         </>
     )
 }
